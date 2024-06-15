8cb79e6938006c7741f8950f7f0012438db09053
mononoken <ken.maeshima@pm.me>
2024-06-14 20:41:22 -0700

    Setup cave man layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,12 @@ import { useState } from "react";
 import "./App.css";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <>
+      <h1>Memory Flags</h1>
+      <div>Score: _</div>
+      <div>Best: _</div>
+      <div>Cards</div>
+    </>
+  );
 }
