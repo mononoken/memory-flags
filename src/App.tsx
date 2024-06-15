8e08ad6e15be383efb1a5c42185b89f7af1ca552
mononoken <ken.maeshima@pm.me>
2024-06-15 02:55:25 -0700

    Fetch 8 random flags for the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Flag } from "./types/Flag";
 import FlagView from "./components/FlagView";
 import MemoryCard from "./components/MemoryCardView";
 
 export default function App() {
-  const [flags, setFlags] = useState<Flag[]>([
-    {
-      id: 1,
-      country: "Belgium",
-      imageUrl: "https://flagsapi.com/BE/flat/64.png",
-    },
-    {
-      id: 2,
-      country: "Japan",
-      imageUrl: "https://flagsapi.com/JP/flat/64.png",
-    },
-    {
-      id: 3,
-      country: "Colombia",
-      imageUrl: "https://flagsapi.com/CO/flat/64.png",
-    },
-  ]);
+  const [flags, setFlags] = useState<Flag[]>([]);
+  const [displayedFlags, setDisplayedFlags] = useState<number[]>([2, 4, 6, 8]);
   const [selectedFlagIds, setSelectedFlagIds] = useState<number[]>([]);
   const [highestScore, setHighestScore] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchFlags = async () => {
+      try {
+        const response = await fetch(
+          "https://restcountries.com/v3.1/all?fields=name,flags",
+        );
+        const data = await response.json();
+        const flags = data.map((country: any, index: number) => {
+          return {
+            id: index,
+            country: country.name.common,
+            imageUrl: country.flags.png,
+          };
+        });
+        setFlags(flags);
+
+        setDisplayedFlags(getRandomFlagIds(flags.length));
+      } catch (error) {
+        console.log("Error fetching flags:", error);
+      }
+    };
+
+    fetchFlags();
+  }, []);
+
+  const getRandomFlagIds = (max: number): number[] => {
+    const randomIds = new Set<number>();
+
+    while (randomIds.size < 8) {
+      const randomFlagId: number = Math.floor(Math.random() * max);
+      randomIds.add(randomFlagId);
+    }
+
+    return Array.from(randomIds);
+  };
 
   const score: number = selectedFlagIds.length;
 
@@ -47,12 +68,16 @@ export default function App() {
       <div>Score: {score}</div>
       <div>Best: {highestScore}</div>
       <div>
-        {flags.map((flag) => (
-          <MemoryCard
-            content={<FlagView key={flag.id} {...flag} />}
-            handleCardSelect={() => handleCardSelect(flag)}
-          />
-        ))}
+        {flags.length !== 0 &&
+          displayedFlags
+            .map((id) => flags[id])
+            .map((flag) => (
+              <MemoryCard
+                key={flag.id}
+                content={<FlagView {...flag} />}
+                handleCardSelect={() => handleCardSelect(flag)}
+              />
+            ))}
       </div>
     </>
   );
